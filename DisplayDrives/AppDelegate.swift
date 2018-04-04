67df90ad2806e830859ee178954b4591063bd5c9
Jake Fry <frywj@yahoo.com>
2018-04-03 21:33:42 -0700

    Initial Commit

---
+++ DisplayDrives/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DisplayDrives
+//
+//  Created by Jake Fry on 4/3/18.
+//  Copyright © 2018 Jake Fry. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
